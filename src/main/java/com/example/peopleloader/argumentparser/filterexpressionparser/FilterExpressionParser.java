e6b75c7165b1c11d698bcb2c164bbf55ee33ec96
Eduardo Muñoz <jesus_eme_ge@hotmail.com>
2017-06-01 22:12:12 +0000

    Comment method

--- src/main/java/com/example/peopleloader/argumentparser/filterexpressionparser/FilterExpressionParser.java
+++ src/main/java/com/example/peopleloader/argumentparser/filterexpressionparser/FilterExpressionParser.java
@@ -1,9 +1,20 @@
 package com.example.peopleloader.argumentparser.filterexpressionparser;
 
+import com.example.peopleloader.argumentparser.filterexpressionparser.exception.InvalidFilterException;
 import com.example.peopleloader.filterexpression.FilterExpression;
 
 public interface FilterExpressionParser {
 
+	/**
+	 * Parses the given expression to create a {@link FilterExpression}
+	 * 
+	 * @param filterExpression
+	 *            the expression to be parsed
+	 * @return the parsed expression
+	 * 
+	 * @throws InvalidFilterException
+	 *             if the expression cannot be parsed
+	 */
 	public FilterExpression parse(String filterExpression);
 
 }
